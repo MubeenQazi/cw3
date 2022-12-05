64b7cb3c385877a779c6c3f2b11d9acd6de5ff33
farzanfaiq <farzanfaiq7@gmail.com>
2022-12-05 22:12:28 +0500

    update

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+            
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title" title="Laragon">Cw3</div>
+                <div class="opt">
+                  <div><a title="Getting Started" href="/demo1/dist/">Goto Dashboard</a></div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
